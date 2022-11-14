971c64c1d2bb8d8e59334a802a4994d4531255ed
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-14 17:36:38 +0100

    library class created

---
+++ src/Library.py
@@ -0,0 +1,19 @@
+from Game import Game
+
+class Library():
+	def __init__(self, path:str) -> None:
+		self.games:list[Game] = []
+		self.path = path # the absolute path to the dir with the game data.
+	
+	def __repr__(self) -> str:
+		str = ''
+		for game in self.games:
+			str += game
+			str += '\n'
+		return str
+
+	def add_game(self) -> None:
+		pass
+
+	def remove_game(self, name:str) -> None:
+		pass
