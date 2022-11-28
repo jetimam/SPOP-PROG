f0cbd8c7932a0d31655995967735a787f3058977
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-28 09:11:26 +0100

    dashboard visualization and filter configuration implemented

--- Dashboard.py
+++ Dashboard.py
@@ -1,14 +1,39 @@
 from Game import Game
-import random
+import csv
 
 class Dashboard():
 	def __init__(self, game:Game, id:int, filters=[]) -> None:
 		self.game = game
-		self.filters = filters
+		self.filters = filters #the indices if the fields we do not want to see
 		self.id = id
 
+	def import_data(self) -> tuple:
+		with open('games/' + self.game, 'r') as csvfile:
+			csvreader = csv.reader(csvfile)
+			fields = next(csvreader)
+			data = next(csvreader)
+			return (fields, data)
+
 	def __repr__(self) -> str:
-		pass
+		fields, data = self.import_data()
+		for i in range(len(fields)):
+			if i in self.filters:
+				fields.pop(i)
+				[data[j].pop(i) for j in range(len(data))]
+		str = ''
+		for i in range(len(fields)):
+			str += str(fields[i]) + ': ' + str(data[i]) + '\n'
+		return str
 
 	def configure_filters(self) -> None:
-		pass
+		print('Choose the index of the filters you wish to remove. Enter them in a single line with spaces separating them.')
+		fields, data = self.import_data()
+		[print(str(i+1) + ': ' + str(data[i])) for i in range(len(fields))]
+		indices = input().split()
+		for i in indices:
+			if type(i) == str:
+				print('Invalid input. Do not enter letters, only numbers.')
+			elif i > len(fields) or i < 1:
+				print('Invalid input. Do not enter an index that is not mentioned above.')
+			else:
+				self.filters = indices
