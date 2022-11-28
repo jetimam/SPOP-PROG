b6c85b029657a8bc045caaa38e47bb9774dcf577
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-28 11:59:48 +0100

    majority of dashboardlibrary class implemented

--- DashboardLibrary.py
+++ DashboardLibrary.py
@@ -1,28 +1,58 @@
 from Dashboard import Dashboard
 
 class DashboardLibrary():
-	def __init__(self, dashboards=[], chosen:Dashboard=None) -> None:
-		self.dashboards = dashboards
-		self.chosen = chosen
+	def __init__(self, dashboards=[], chosen=None) -> None:
+		self.dashboards:list[Dashboard] = dashboards
+		self.chosen:Dashboard = chosen
 
 	def __repr__(self) -> str:
 		str = ''
+		c = 0
 		for dashboard in self.dashboards:
-			str += dashboard
-			str += '\n'
+			fields, _ = dashboard.import_data()
+			fields_str = ''
+			for i in range(fields):
+				if i in dashboard.filters:
+					fields_str += fields[i] + ' '
+			str += str(c+1) + ') ' + 'Game: ' + dashboard.game + '\n' + 'Filters Applied: ' + fields_str + '\n'
 		return str
 
 	def choose(self) -> None:
-		pass
+		print('Please choose your main dashboard by entering the index of the dashboard you want.\n' + self)
+		s = input()
+		if not s.isnumeric():
+			print('Invalid input. Enter only numbers.')
+		else:
+			s = int(s)-1
+			if s > len(self.dashboards) or s < 1:
+				print('Invalid input. Do not enter an index that is not mentioned above.')
+			else:
+				self.chosen = self.dashboards[s]
+				print('New dashboard chosen!')
 
 	def view_chosen(self) -> None:
-		print(self.chosen)
+		fields, _ = self.chosen.import_data()
+		fields_str = ''
+		for i in range(fields):
+			if i in self.chosen.filters:
+				fields_str += fields[i] + ' '
+		print('Game: ' + self.chosen.game + '\n' + 'Filters Applied: ' + fields_str)
 
 	def add(self, dashboard:Dashboard) -> None:
 		pass
 
 	def remove(self, index:int) -> None:
-		pass
+		print('Please choose which dashboard you wish to remove.\n' + self)
+		s = input()
+		if not s.isnumeric():
+			print('Invalid input. Enter only numbers.')
+		else:
+			s = int(s)-1
+			if s > len(self.dashboards) or s < 1:
+				print('Invalid input. Do not enter an index that is not mentioned above.')
+			else:
+				self.dashboards.pop(s)
+				print('Dashboard removed.')
 
 	def get_index(self, name:str) -> int:
 		for i in range(len(self.dashboards)):
