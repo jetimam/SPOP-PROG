0b6e5f1042b7c5b8e55db622a87e96d99ef59a34
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-28 13:44:00 +0100

    dashboard class improved

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,4 @@
 from Game import Game
-import csv
 
 class Dashboard():
 	def __init__(self, game:Game, id:int, filters=[]) -> None:
@@ -7,28 +6,19 @@ class Dashboard():
 		self.filters = filters #the indices if the fields we do not want to see
 		self.id = id
 
-	def import_data(self) -> tuple(list[str], list[str]):
-		with open('games/' + self.game, 'r') as csvfile:
-			csvreader = csv.reader(csvfile)
-			fields = next(csvreader)
-			data = next(csvreader)
-			return (fields, data)
-
 	def __repr__(self) -> str:
-		fields, data = self.import_data()
-		for i in range(len(fields)):
+		for i in range(len(self.game.fields)):
 			if i in self.filters:
-				fields.pop(i)
-				[data[j].pop(i) for j in range(len(data))]
+				self.game.fields.pop(i)
+				[self.game.data[j].pop(i) for j in range(len(self.game.data))]
 		str = ''
-		for i in range(len(fields)):
-			str += str(fields[i]) + ': ' + str(data[i]) + '\n'
+		for i in range(len(self.game.fields)):
+			str += str(self.game.fields[i]) + ': ' + str(self.game.data[i]) + '\n'
 		return str
 
 	def configure_filters(self) -> None:
 		print('Choose the index of the filters you wish to remove. Enter them in a single line with spaces separating them.')
-		fields, data = self.import_data()
-		[print(str(i+1) + ') ' + str(data[i])) for i in range(len(fields))]
+		[print(str(i+1) + ') ' + str(self.game.data[i])) for i in range(len(self.game.fields))]
 		indices = input().split()
 		[indices.pop(i) for i in range(len(indices)-1, 0, -1) if indices.count(indices[i]) > 1] #remove dupes
 		success = True
@@ -39,7 +29,7 @@ class Dashboard():
 				break
 			else:
 				indices[i] = int(indices[i])-1
-				if indices[i] > len(fields) or indices[i] < 1:
+				if indices[i] > len(self.game.fields) or indices[i] < 1:
 					print('Invalid input. Do not enter an index that is not mentioned above.')
 					success = False
 					break
