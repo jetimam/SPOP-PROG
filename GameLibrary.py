0c76a636ec1f4c2acd1339442a5088c5fc9b6576
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-28 13:44:50 +0100

    gamelibrary class implemented

--- GameLibrary.py
+++ GameLibrary.py
@@ -1,8 +1,9 @@
 from Game import Game
+from os import listdir
 
 class GameLibrary():
 	def __init__(self, games=[]) -> None:
-		self.games = games
+		self.games:list[Game] = games
 	
 	def __repr__(self) -> str:
 		str = ''
@@ -12,7 +13,20 @@ class GameLibrary():
 		return str
 
 	def add(self, name:str) -> None:
-		pass
+		if name+'.csv' in listdir('games/'):
+			for game in self.games: #overriding previous game entry with the new game entry
+				if game.name == name:
+					self.games.pop(self.games.index(game))
+					break
+			self.games.append(Game(name))
+			print(name + ' added!')
+		else:
+			print(name + ' couldn\'t be found.')
 
-	def remove(self, index:int) -> None:
-		pass
+	def remove(self, name:str) -> None:
+		for game in self.games:
+			if game.name == name:
+				self.games.pop(self.games.index(game))
+				print(name + ' removed.')
+				return None
+		print(name + ' couldn\'t be found.')
