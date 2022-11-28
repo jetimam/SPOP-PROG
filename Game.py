a25bdd8da28e71ef3192c69eed1e38c20b4824d3
Matt Umut Ucak <umutucak@protonmail.com>
2022-11-28 13:43:45 +0100

    game class implemented

--- Game.py
+++ Game.py
@@ -1,8 +1,16 @@
+import csv
+
 class Game():
-	def __init__(self, name:str, headers, data) -> None:
+	def __init__(self, name:str) -> None:
 		self.name = name
-		self.headers = headers
-		self.data = data
+		self.fields, self.data = self.import_data()
 
 	def __repr__(self) -> str:
 		return self.name
+
+	def import_data(self) -> tuple(list[str], list[str]):
+		with open('games/' + self.name, 'r') as csvfile:
+			csvreader = csv.reader(csvfile)
+			fields = next(csvreader)
+			data = next(csvreader)
+			return (fields, data)
